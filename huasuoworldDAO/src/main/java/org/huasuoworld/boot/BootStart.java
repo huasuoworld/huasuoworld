f4a6f9eaf3d278996922079e38524b525c095366
huasuoworld@outlook.com <huasuoworld@outlook.com>
2017-05-14 20:11:24 +0800

    pom config

--- huasuoworldDAO/src/main/java/org/huasuoworld/boot/BootStart.java
+++ huasuoworldDAO/src/main/java/org/huasuoworld/boot/BootStart.java
@@ -2,14 +2,11 @@ package org.huasuoworld.boot;
 
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
-
 @MapperScan("org.huasuoworld.dao")
 @ComponentScan("org.huasuoworld.controller")
-@EnableAutoConfiguration
 @SpringBootApplication
 public class BootStart {
 
